e9edcf62047d0fff8a71c0b9ef774026a608512a
rahul3006soni <rahul.sonicse2024@indoreinstitute.com>
2025-09-15 11:33:47 +0530

    DSA program for reverse a digit

---
+++ reverse number dsa.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int num, reversed = 0;
+printf("Enter a number: ");
+    scanf("%d", &num);
+    for(;num!=0;num/=10)
+    {
+    	reversed=reversed*10+num%10;
+	}
+	printf("reversed number is: %d\n", reversed);
+	return 0;
+}
